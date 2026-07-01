591be0b10451a78b2e1e59c1004c156e578c4a8f
bandishdudhagara2108-ai <bandishdudhagara2108@gmail.com>
2026-07-02 01:41:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function calculateWeight() {
 
+    let density = parseFloat(document.getElementById("material").value);
+
+    let diameter = parseFloat(document.getElementById("diameter").value);
+
+    let length = parseFloat(document.getElementById("length").value);
+
+    if (isNaN(diameter) || isNaN(length)) {
+        alert("Please enter Diameter and Length.");
+        return;
+    }
+
+    // mm → cm
+    diameter = diameter / 10;
+    length = length / 10;
+
+    // Weight in kg
+    let weight = (Math.PI / 4) * diameter * diameter * length * density / 1000;
+
+    document.getElementById("result").innerHTML =
+        "Weight : " + weight.toFixed(3) + " kg";
+}
